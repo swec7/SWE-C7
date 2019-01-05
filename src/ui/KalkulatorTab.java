c8e27285f3f2a3c7fac28b2d359e49adfd02d34e
Kyubai <mrkyubai@gmail.com>
2019-01-05 21:28:20 +0100

    refactored UI Code, added ResizePolicy to eliminate additional columns, changed Table background colour

--- src/ui/KalkulatorTab.java
+++ src/ui/KalkulatorTab.java
@@ -26,91 +26,96 @@ import javafx.scene.text.Text;
 import javafx.util.Callback;
 
 public class KalkulatorTab extends QisTab {
-	
-	//	Testdaten 	------------------------------------------------------------------------------
-	Modul bestanden = new Modul(1, "Gip", 8, 1.6f, 2, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1),1,Typ.PFLICHT, 0);
-	Modul durchgefallen = new Modul(2, "dnis", 8, 5.0f, 1, null, LocalDate.of(2018, 1, 1),2,Typ.PFLICHT, 0);
-	Modul offen = new Modul(3, "TI", 8, 0, 0, null, null,1,Typ.PFLICHT, 0);
-	//	------------------------------------------------------------------------------------------
 
-	ArrayList<Modul> daten = new ArrayList<Modul>(Arrays.asList(bestanden,durchgefallen,offen));
+    //DEBUG ----------------------------------------------------------
+    //Testdaten follows
+    Modul m1 = new Modul(51101, "Hoehere Mathematik 1", 8, 3.0f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 1, Typ.PFLICHT, 0);
+    Modul m2 = new Modul(51104, "Grundlagen der Informatik und hoehere Programmiersprache fuer Informatik", 11, 1.3f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 1, Typ.PFLICHT, 0);
+    Modul m3 = new Modul(55667, "Kommunikationstechnicken", 2, 0.0f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 2, Typ.SOFTSKILL, 0);
+    Modul m4 = new Modul(55668, "Wissenschaftliches Arbeiten", 2, 0.0f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 1, Typ.SOFTSKILL, 0);
+    Modul m5 = new Modul(52105, "Technische Informatik", 2, 1.3f, 2, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 1, Typ.PFLICHT, 0);
+    Modul m6 = new Modul(55667, "Algorithmen und Datenstrukturen", 8, -1.0f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 2, Typ.PFLICHT, 0);
+    //END DEBUG ------------------------------------------------------
 
-	public KalkulatorTab(Benutzer benutzer){
-		
-		TableView<Modul> kalkulator = new TableView<Modul>();
-		GridPane.setMargin(kalkulator, new Insets(0, 60, 0, 0));
-		GridPane.setHgrow(kalkulator, Priority.ALWAYS);
-		GridPane.setVgrow(kalkulator, Priority.ALWAYS);
-		kalkulator.setEditable(true);
-		kalkulator.getStyleClass().add("grey");
-		
-		TableColumn<Modul, String> modulname = new TableColumn<Modul, String>("Modulname");
-		TableColumn<Modul, Integer> credits = new TableColumn<Modul, Integer>("Credits");
-		TableColumn<Modul, Float> note = new TableColumn<Modul, Float>("Note");
-		
-		kalkulator.getColumns().addAll(modulname,credits,note);
-		
-		modulname.setCellValueFactory(new PropertyValueFactory<>("name"));
-		credits.setCellValueFactory(new PropertyValueFactory<>("credits"));
-		note.setCellValueFactory(new PropertyValueFactory<Modul, Float>("note"));
-		note.setCellFactory((TableColumn<Modul, Float> param) -> new EditingCell());
-		note.setOnEditCommit((TableColumn.CellEditEvent<Modul,Float> e)-> {
-			((Modul) e.getTableView().getItems().get(e.getTablePosition().getRow())).setPlanNote(e.getNewValue());
-		});
-		kalkulator.setItems(FXCollections.observableArrayList(daten));
-		
-		
-		Text aktDurchschnittTx = new Text("Aktuelle Durchschnittsnote:\t");
-		aktDurchschnittTx.getStyleClass().add("tabtext");
-		Text errechDurchschnittTx = new Text("Errechnete Durchschnitsnote:\t");
-		errechDurchschnittTx.getStyleClass().add("tabtext");
-		Text verblVersuche = new Text("Verbleibende Verbesserungsversuche:\t");
-		verblVersuche.getStyleClass().add("tabtext");
-		
-		Button reset = new Button("Reset");
-		reset.setOnAction(e->{
-			//Daten aus TableView-> Plannote auf note setzen
-			//Update items in TableView
-			//Durchschnitt neu berechnen 
-			//Text aktualisieren
-		});
-		
-		HBox wunschnoteBox = new HBox();
-		wunschnoteBox.getStyleClass().add("border-top");
-		wunschnoteBox.setAlignment(Pos.CENTER);
-		Text wunschnoteRechTx = new Text("Wunschnotenenrechner");
-		wunschnoteRechTx.getStyleClass().addAll("zueberschrift");
-		wunschnoteBox.getChildren().add(wunschnoteRechTx);
-		
-		
-		Text wunschnoteTx = new Text("Wunschnote");
-		wunschnoteTx.getStyleClass().add("tabtext");
-		
-		TextField wunschnoteTf = new TextField();
-		
-		Text wunschnoteNichtBerTx = new Text("deine Wunschnote ist nicht ohne Verbesserungsversuch erreichbar");
-		GridPane.setHalignment(wunschnoteNichtBerTx, HPos.CENTER);
-		
-		Button wunschnoteBer = new Button("OK");
-		wunschnoteBer.setOnAction(e->{
-			//Berschnungsmethode Hier
-		});
-		
-		
-		this.add(kalkulator, 0, 0, 4, 1);
-		this.add(aktDurchschnittTx, 0, 1, 2, 1);
-		this.add(errechDurchschnittTx, 0, 2, 2, 1);
-		this.add(reset, 2, 1, 1, 2);
-		
-		this.add(verblVersuche, 0, 3, 2, 1);
-		
-		this.add(wunschnoteBox, 0, 4, 3, 1);
-		this.add(wunschnoteTx, 0, 5);
-		this.add(wunschnoteTf, 1, 5);
-		this.add(wunschnoteBer, 2, 5);
-		this.add(wunschnoteNichtBerTx, 0, 6, 3, 1);
-		
-		
+    ArrayList<Modul> daten = new ArrayList<>(Arrays.asList(m1,m2,m3,m4,m5,m6));
+
+    public KalkulatorTab(Benutzer benutzer){
+
+        //Output Texte des Tabs
+        Text aktDurchschnittTx = new Text("Aktuelle Durchschnittsnote:\t");
+        Text errechDurchschnittTx = new Text("Errechnete Durchschnittsnote:\t");
+        Text verblVersuche = new Text("Verbleibende Verbesserungsversuche:\t");
+        Text wunschnoteRechTx = new Text("Wunschnotenenrechner");
+        Text wunschnoteTx = new Text("Wunschnote");
+        Text wunschnoteNichtBerTx = new Text("deine Wunschnote ist nicht ohne Verbesserungsversuche erreichbar");
+        TextField wunschnoteTf = new TextField();
+        //CSS Styles der Texte
+        aktDurchschnittTx.getStyleClass().add("tabtext");
+        errechDurchschnittTx.getStyleClass().add("tabtext");
+        verblVersuche.getStyleClass().add("tabtext");
+        wunschnoteRechTx.getStyleClass().addAll("zueberschrift");
+        wunschnoteTx.getStyleClass().add("tabtext");
+        //Properties der Texte
+        GridPane.setHalignment(wunschnoteNichtBerTx, HPos.CENTER);
+
+        //Tabelle
+        TableView<Modul> kalkulator = new TableView<>();
+        //CSS Styles der Tabelle
+        kalkulator.getStyleClass().add("grey");
+        //Colums der Tabelle
+        TableColumn<Modul, String> modulname = new TableColumn<>("Modulname");
+        TableColumn<Modul, Integer> credits = new TableColumn<>("Credits");
+        TableColumn<Modul, Float> note = new TableColumn<>("Note");
+        kalkulator.getColumns().addAll(modulname,credits,note);
+        //Properties der Tabelle
+        GridPane.setMargin(kalkulator, new Insets(0, 60, 0, 0));
+        GridPane.setHgrow(kalkulator, Priority.ALWAYS);
+        GridPane.setVgrow(kalkulator, Priority.ALWAYS);
+        kalkulator.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        kalkulator.setEditable(true);
+
+        modulname.setCellValueFactory(new PropertyValueFactory<>("name"));
+        credits.setCellValueFactory(new PropertyValueFactory<>("credits"));
+        note.setCellValueFactory(new PropertyValueFactory<>("note"));
+        note.setCellFactory((TableColumn<Modul, Float> param) -> new EditingCell());
+        note.setOnEditCommit((TableColumn.CellEditEvent<Modul,Float> e)->  (e.getTableView().getItems().get(e.getTablePosition().getRow())).setPlanNote(e.getNewValue()));
+        kalkulator.setItems(FXCollections.observableArrayList(daten));
+
+
+        Button reset = new Button("Reset");
+        reset.setOnAction(e->{
+            //TODO Daten aus TableView-> Plannote auf note setzen
+            //Update items in TableView
+            //Durchschnitt neu berechnen
+            //Text aktualisieren
+        });
+
+        HBox wunschnoteBox = new HBox();
+        wunschnoteBox.getStyleClass().add("border-top");
+        wunschnoteBox.setAlignment(Pos.CENTER);
+
+        wunschnoteBox.getChildren().add(wunschnoteRechTx);
+
+
+        Button wunschnoteBer = new Button("OK");
+        wunschnoteBer.setOnAction(e->{
+            //TODO Berechnungsmethode Hier
+        });
+
+        this.add(kalkulator, 0, 0, 4, 1);
+        this.add(aktDurchschnittTx, 0, 1, 2, 1);
+        this.add(errechDurchschnittTx, 0, 2, 2, 1);
+        this.add(reset, 2, 1, 1, 2);
+
+        this.add(verblVersuche, 0, 3, 2, 1);
+
+        this.add(wunschnoteBox, 0, 4, 3, 1);
+        this.add(wunschnoteTx, 0, 5);
+        this.add(wunschnoteTf, 1, 5);
+        this.add(wunschnoteBer, 2, 5);
+        this.add(wunschnoteNichtBerTx, 0, 6, 3, 1);
+
+
 //		ColumnConstraints first = new ColumnConstraints();
 //		first.setHgrow(Priority.NEVER);
 //		ColumnConstraints second = new ColumnConstraints();
@@ -119,86 +124,72 @@ public class KalkulatorTab extends QisTab {
 //		third.setHgrow(Priority.NEVER);
 //		ColumnConstraints fourth = new ColumnConstraints();
 //		fourth.setHgrow(Priority.ALWAYS);
-//		
-//		
-//		
+
 //		this.getColumnConstraints().addAll(first,second,third,fourth);
-		
-		
+//
 //		this.setGridLinesVisible(true);
-//		
+//
 //		GridPane.setHgrow(wunschnoteNichtBerTx, Priority.ALWAYS);
-		
-		
-	}
-	
-	
-	 
-	 class EditingCell extends TableCell<Modul, Float> {
+    }
 
-	        private TextField textField;
+    class EditingCell extends TableCell<Modul, Float> {
+        private TextField textField;
 
-	        private EditingCell() {
-	        }
+        private EditingCell() {}
 
-	        @Override
-	        public void startEdit() {
-	            if (!isEmpty()) {
-	                super.startEdit();
-	                createTextField();
-	                setText(null);
-	                setGraphic(textField);
-	                textField.selectAll();
-	            }
-	        }
+        @Override
+        public void startEdit() {
+            if (!isEmpty()) {
+                super.startEdit();
+                createTextField();
+                setText(null);
+                setGraphic(textField);
+                textField.selectAll();
+            }
+        }
 
-	        @Override
-	        public void cancelEdit() {
-	            super.cancelEdit();
+        @Override
+        public void cancelEdit() {
+            super.cancelEdit();
+            setText( getItem().toString());
+            setGraphic(null);
+        }
 
-	            setText( getItem().toString());
-	            setGraphic(null);
-	        }
-
-	        @Override
-	        public void updateItem(Float item, boolean empty) {
-	            super.updateItem(item, empty);
-
-	            if (empty|| item.equals(0.0f)) {
+        @Override
+        public void updateItem(Float item, boolean empty) {
+            super.updateItem(item, empty);
+            if (empty|| item.equals(0.0f)) {
 //	                setText(item.toString());
-	                setText("");
-	                setGraphic(null);
-	            } else {
-	                if (isEditing()) {
-	                    if (textField != null) {
-	                        textField.setText(getString());
+                setText("");
+                setGraphic(null);
+            } else {
+                if (isEditing()) {
+                    if (textField != null) {
+                        textField.setText(getString());
 //	                        setGraphic(null);
-	                    }
-	                    setText(null);
-	                    setGraphic(textField);
-	                } else {
-	                    setText(getString());
-	                    setGraphic(null);
-	                }
-	            }
-	        }
+                    }
+                    setText(null);
+                    setGraphic(textField);
+                } else {
+                    setText(getString());
+                    setGraphic(null);
+                }
+            }
+        }
 
-	        private void createTextField() {
-	            textField = new TextField(getString());
-	            textField.setMinWidth(this.getWidth() - this.getGraphicTextGap() * 2);
-	            textField.setOnAction((e) -> commitEdit(Float.parseFloat(textField.getText())));
-	            textField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-	                if (!newValue) {
+        private void createTextField() {
+            textField = new TextField(getString());
+            textField.setMinWidth(this.getWidth() - this.getGraphicTextGap() * 2);
+            textField.setOnAction((e) -> commitEdit(Float.parseFloat(textField.getText())));
+            textField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+                if (!newValue) {
 //	                    System.out.println("Commiting " + textField.getText());
-	                    commitEdit(Float.parseFloat(textField.getText()));
-	                }
-	            });
-	        }
-
-	        private String getString() {
-	            return getItem() == null ? "" : getItem().toString();
-	        }
-	}
-	
-	
+                    commitEdit(Float.parseFloat(textField.getText()));
+                }
+            });
+        }
+        private String getString() {
+            return getItem() == null ? "" : getItem().toString();
+        }
+    }
 }
--- src/ui/UebersichtTab.java
+++ src/ui/UebersichtTab.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import daten.Benutzer;
 import daten.Modul;
 import daten.Typ;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.scene.control.TableCell;
@@ -19,132 +20,140 @@ import javafx.scene.layout.Priority;
 import javafx.scene.text.Text;
 
 public class UebersichtTab extends QisTab{
-	SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
-	private float notenspiegel = 1.6f;
-	
-	
+	//SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
 	public UebersichtTab(Benutzer benutzer){
+		//DEBUG ----------------------------------------------------------
+		//Data from parser follows
+		float durchschnittsnote = 1.6f;
+		//Testdaten follows
+		Modul m1 = new Modul(51101, "Hoehere Mathematik 1", 8, 3.0f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 1, Typ.PFLICHT, 0);
+		Modul m2 = new Modul(51104, "Grundlagen der Informatik und hoehere Programmiersprache fuer Informatik", 11, 1.3f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 1, Typ.PFLICHT, 0);
+		Modul m3 = new Modul(55667, "Kommunikationstechnicken", 2, 0.0f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 2, Typ.SOFTSKILL, 0);
+		Modul m4 = new Modul(55668, "Wissenschaftliches Arbeiten", 2, 0.0f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 1, Typ.SOFTSKILL, 0);
+		Modul m5 = new Modul(52105, "Technische Informatik", 2, 1.3f, 2, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 1, Typ.PFLICHT, 0);
+		Modul m6 = new Modul(55667, "Algorithmen und Datenstrukturen", 8, -1.0f, 1, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1), 2, Typ.PFLICHT, 0);
+		//END DEBUG ------------------------------------------------------
+
 		this.setPadding(new Insets(20, 20, 60, 20));
-		
-		TableView<Modul> uebersicht = new TableView<>();   
-		
-		GridPane.setMargin(uebersicht, new Insets(0, 60, 0, 0)); 
-		
-		TableColumn<Modul, String> modulname = new TableColumn<Modul, String>("Modulname");
-		TableColumn<Modul, Integer> semester = new TableColumn<Modul, Integer>("Semester");
-		TableColumn<Modul, Integer> credits = new TableColumn<Modul, Integer>("Credits");
-		TableColumn<Modul, Float> note = new TableColumn<Modul, Float>("Note");
-		TableColumn<Modul, LocalDate> ablaufdatum = new TableColumn<Modul, LocalDate>("Ablaufdatum");
-		TableColumn<Modul, Integer> versuch = new TableColumn<Modul, Integer>("Versuch");
-		
+
+		//Texte des Tabs
+		Text schnitt = new Text("Aktuelle Durchschnittsnote:\t"+durchschnittsnote);
+		//CSS Styles der Texte
+		schnitt.getStyleClass().add("tabtext");
+
+		//Tabelle
+		TableView<Modul> uebersicht = new TableView<>();
+		//Columns der Tabelle
+		TableColumn<Modul, String> modulname = new TableColumn<>("Modulname");
+		TableColumn<Modul, Integer> semester = new TableColumn<>("Semester");
+		TableColumn<Modul, Integer> credits = new TableColumn<>("Credits");
+		TableColumn<Modul, Float> note = new TableColumn<>("Note");
+		TableColumn<Modul, LocalDate> ablaufdatum = new TableColumn<>("Ablaufdatum");
+		TableColumn<Modul, Integer> versuch = new TableColumn<>("Versuch");
 		uebersicht.getColumns().addAll(modulname, semester, credits, note, ablaufdatum, versuch);
-		
+		//Properties der Tabelle
+		GridPane.setMargin(uebersicht, new Insets(0, 60, 0, 0));
+//        semester.prefWidthProperty().bind(new SimpleIntegerProperty(50));
+//        credits.prefWidthProperty().bind(new SimpleIntegerProperty(50));
+//        note.prefWidthProperty().bind(new SimpleIntegerProperty(50));
+//        ablaufdatum.prefWidthProperty().bind(new SimpleIntegerProperty(50));
+//        versuch.prefWidthProperty().bind(new SimpleIntegerProperty(50));
+		GridPane.setHgrow(uebersicht, Priority.ALWAYS);
+		GridPane.setVgrow(uebersicht, Priority.ALWAYS);
+		uebersicht.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		//Tabelle set Items
+		//TODO Use Parser Data
+		uebersicht.setItems( FXCollections.observableArrayList(m1,m2,m3,m4,m5,m6));
+
 		modulname.setCellValueFactory(new PropertyValueFactory<>("name"));
 		semester.setCellValueFactory(new PropertyValueFactory<>("semester"));
 		credits.setCellValueFactory(new PropertyValueFactory<>("credits"));
 		note.setCellValueFactory(new PropertyValueFactory<>("note"));
 		note.setCellFactory((TableColumn<Modul, Float> column) -> {
-			   return new TableCell<Modul, Float>() {
-			      @Override
-			      protected void updateItem(Float item, boolean empty) {
-			         super.updateItem(item, empty);
-			         if (item == null || item == 0 || empty) {
-			            setText(null);
-			         } else {
-			            setText(item.toString());
+			return new TableCell<Modul, Float>() {
+				@Override
+				protected void updateItem(Float item, boolean empty) {
+					super.updateItem(item, empty);
+					if (item == null || item == 0 || empty) {
+						setText(null);
+					} else {
+						setText(item.toString());
 //			            if(item == 5.0f){
 //			            	getTableRow().getStyleClass().setAll("rowRed");
 //			            }else if(item>=1.0f){
 //			            	getTableRow().getStyleClass().setAll("rowGreen");
 //			            }
-			        }
-			     }
-			   };
-			});
+					}
+				}
+			};
+		});
 		ablaufdatum.setCellValueFactory(new PropertyValueFactory<>("ablaufdatum"));
 		ablaufdatum.setCellFactory((TableColumn<Modul, LocalDate> column) -> {
-			   return new TableCell<Modul, LocalDate>() {
-			      @Override
-			      protected void updateItem(LocalDate item, boolean empty) {
-			         super.updateItem(item, empty);
-			         if (item == null || empty) {
-			            setText(null);
-			         } else {
-			            setText(item.getDayOfMonth()+"."+ item.getMonthValue()+"."+item.getYear());
-			        }
-			     }
-			   };
-			});
+			return new TableCell<Modul, LocalDate>() {
+				@Override
+				protected void updateItem(LocalDate item, boolean empty) {
+					super.updateItem(item, empty);
+					if (item == null || empty) {
+						setText(null);
+					} else {
+						setText(item.getDayOfMonth()+"."+ item.getMonthValue()+"."+item.getYear());
+					}
+				}
+			};
+		});
 		versuch.setCellValueFactory(new PropertyValueFactory<>("versuche"));
 		versuch.setCellFactory((TableColumn<Modul, Integer> column) -> {
-			   return new TableCell<Modul, Integer>() {
-			      @Override
-			      protected void updateItem(Integer item, boolean empty) {
-			         super.updateItem(item, empty);
-			         if (item == null || item == 0 || empty) {
-			            setText(null);
+			return new TableCell<Modul, Integer>() {
+				@Override
+				protected void updateItem(Integer item, boolean empty) {
+					super.updateItem(item, empty);
+					if (item == null || item == 0 || empty) {
+						setText(null);
 //			            getTableRow().getStyleClass().setAll("rowGrey");
-			         } else {
-			            setText(item.toString());
-			        }
-			     }
-			   };
-			});
-		
-		semester.setSortType(TableColumn.SortType.ASCENDING);
-		
-		
-		uebersicht.setRowFactory(row -> new TableRow<Modul>(){
-			 @Override
-			    public void updateItem(Modul item, boolean empty){
-			        super.updateItem(item, empty);
-
-			        if (item == null || empty) {
-			        	getStyleClass().remove("rowGreen");
-			        	getStyleClass().remove("rowRed");
-			        	getStyleClass().add("rowGrey");
-			        }else if(item.getNote()==0){
-			        	getStyleClass().remove("rowGreen");
-			        	getStyleClass().remove("rowRed");
-			        	getStyleClass().add("rowGrey");
-			        }else if(item.getNote()==5.0f){
-			        	getStyleClass().remove("rowGreen");
-			        	getStyleClass().remove("rowGrey");
-			        	getStyleClass().add("rowRed");
-			        }else{
-			        	getStyleClass().remove("rowGrey");
-			        	getStyleClass().remove("rowRed");
-			        	getStyleClass().add("rowGreen");
-			        }
-			    }
-			
-			
+					} else {
+						setText(item.toString());
+					}
+				}
+			};
 		});
-		LocalDate d = LocalDate.of(2019, 1, 1);
-//		Testdaten 	------------------------------------------------------------------------------
-		Modul bestanden = new Modul(1, "Gip", 8, 1.6f, 2, LocalDate.of(2019, 1, 1), LocalDate.of(2018, 1, 1),1,Typ.PFLICHT, 0);
-		Modul durchgefallen = new Modul(2, "dnis", 8, 5.0f, 1, null, LocalDate.of(2018, 1, 1),2,Typ.PFLICHT, 0);
-		Modul offen = new Modul(3, "TI", 8, 0, 0, null, null,1,Typ.PFLICHT, 0);
-		
-		uebersicht.setItems( FXCollections.observableArrayList(bestanden, durchgefallen, offen));
-//		------------------------------------------------------------------------------------------
+
+		semester.setSortType(TableColumn.SortType.ASCENDING);
+
+
+		uebersicht.setRowFactory(row -> new TableRow<Modul>(){
+			@Override
+			public void updateItem(Modul item, boolean empty){
+				super.updateItem(item, empty);
+
+				if (item == null || empty) {
+					getStyleClass().remove("rowGreen");
+					getStyleClass().remove("rowRed");
+					getStyleClass().add("rowGrey");
+				}else if(item.getNote()==0){
+					getStyleClass().remove("rowGreen");
+					getStyleClass().remove("rowRed");
+					getStyleClass().add("rowGrey");
+				}else if(item.getNote()==2.0f){
+					//TODO Use Durchschnittsnote
+					getStyleClass().remove("rowGreen");
+					getStyleClass().remove("rowGrey");
+					getStyleClass().add("rowRed");
+				}else{
+					getStyleClass().remove("rowGrey");
+					getStyleClass().remove("rowRed");
+					getStyleClass().add("rowGreen");
+				}
+			}
+
+
+		});
+		//LocalDate d = LocalDate.of(2019, 1, 1);
 		uebersicht.getSortOrder().add(semester);
-		
-		GridPane.setHgrow(uebersicht, Priority.ALWAYS);
-		GridPane.setVgrow(uebersicht, Priority.ALWAYS);
+
 		this.add(uebersicht, 0, 0);
-		
-		
-		Text schnitt = new Text("Aktuelle Durchschnittsnote:\t"+notenspiegel);
-		schnitt.getStyleClass().add("tabtext");
-		
 		this.add(schnitt, 0, 1);
-		
-		
-		
-		
 //		this.setGridLinesVisible(true);
-		
+
 	}
-	
+
 }
